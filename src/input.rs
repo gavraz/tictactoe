78b9a16a3d72c5fb679f0b7488b5af6f0a56c30d
gavraz <gavra@gmail.com>
2024-11-22 15:44:16 +0200

    Introduce input trait

--- src/input.rs
+++ src/input.rs
@@ -1,6 +1,15 @@
 use std::io;
 
-fn parse_input(input: &str) -> Result<(usize, usize), std::num::ParseIntError> {
+pub enum Result {
+    Position(usize, usize),
+    Exit
+}
+
+pub trait Input {
+    fn get(&self) -> Result;
+}
+
+fn parse_input(input: &str) -> std::result::Result<(usize, usize), std::num::ParseIntError> {
     let mut parts = input.trim().split(',');
 
     let i: usize = parts.next().unwrap().parse()?;
@@ -9,18 +18,25 @@ fn parse_input(input: &str) -> Result<(usize, usize), std::num::ParseIntError> {
     Ok((i, j))
 }
 
-pub fn input() -> (usize, usize) {
-    let mut input = String::new();
+pub struct TerminalInput {}
 
-    loop {
-        input.clear();
+impl Input for TerminalInput {
+    fn get(&self) -> Result {
+        let mut input = String::new();
+        loop {
+            input.clear();
 
-        println!("Choose a position (Format: i,j):");
-        io::stdin().read_line(&mut input).expect("failed to read from stdin");
+            println!("Choose a position (Format: i,j):");
+            io::stdin().read_line(&mut input).expect("failed to read from stdin");
 
-        match parse_input(&input) {
-            Ok(result) => break result,
-            Err(e) => println!("incorrect input: {}", e),
+            if input.trim_end() == "quit" {
+                return Result::Exit;
+            }
+
+            match parse_input(&input) {
+                Ok((i, j)) => break Result::Position(i,j),
+                Err(e) => println!("incorrect input: {}", e),
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,16 @@ use crate::game::{Game, GameResult};
 
 fn main() {
     let mut game: Game = Game::new();
-    let display: &mut dyn display::Display = &mut display::tui::TuiDisplay::new();
-
+    let display: &mut dyn display::Display = &mut display::term::TerminalDisplay::new();
+    let input: &mut dyn input::Input = &mut input::TerminalInput{};
+    
     loop {
         display.draw_board(game.state());
 
-        let (i, j) = input::input();
+        let (i,j) = match input.get() {
+            input::Result::Position(i, j) => (i,j),
+            input::Result::Exit => return,
+        };
 
         let move_result = game.apply(i, j);
         display.on_move(move_result);
